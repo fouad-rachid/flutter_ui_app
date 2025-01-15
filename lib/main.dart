c638d884c2e871dc34fd8d13a6b806d036c89692
fouad.rachid <fouad.rachid@moneyticway.com>
2025-01-15 10:29:43 +0100

    first change

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:fl_test_app/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Popppins'),
+      home: const HomePage(),
+    );
+  }
+}
